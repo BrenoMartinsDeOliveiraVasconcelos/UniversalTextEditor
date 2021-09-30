561dceb2df15c21f8b0995da0c38f0be8408feda
Breno Martins <crdj161@gmail.com>
2021-09-30 13:12:04 -0300

    Mesnagem de saida ao apertar X alterada

--- lib/events.py
+++ lib/events.py
@@ -1,16 +1,18 @@
 import tkinter as tk
 from lib import menus
-from tkinter import  messagebox
+from lib.consoledb import consoledb
+from tkinter import messagebox
 from sys import exit
 
 
 def close(root: tk.Tk, text: tk.Text):
-    confirma = messagebox.askyesnocancel("Exit", "Do you want to save "
-                                                 "before exiting?")
-    if confirma is True:
+    confirma = messagebox.askyesnocancel("Exit", "Do you want to exit "
+                                                 "without saving?")
+    consoledb("Close", confirma)
+    if confirma is False:
         menus.saveas(text, root)
         exit(0)
-    elif confirma is False:
+    elif confirma is True:
         exit(0)
     elif confirma is None:
         return
