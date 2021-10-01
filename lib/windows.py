d57b60bcfe02a090aa33fd98fb89c440db28d1b6
Breno Martins <crdj161@gmail.com>
2021-10-01 18:32:38 -0300

    Criado prototipo de layout do gerenciador de notas

--- lib/windows.py
+++ lib/windows.py
@@ -9,12 +9,20 @@ textui = mainui["text"]
 def notemanager(path: str, nots: list):
     nm = tk.Tk()
     utils.windowmaker(nm, "Note Manager")
+    row = 0
+    column = -1
 
     for i in nots:
+        column += 1
+        if column % 10 == 0:
+            row += 1
+            column = 0
+
         text = tk.Text(nm, bg=textui["bg"], fg=textui["fg"],
                        font=("Segoe", 10), wrap="word", undo=True,
                        width=20, height=10, insertbackground=textui["ibg"],
                        selectbackground=textui["sbg"])
-        text.grid(row=1, column=0, rowspan=1, columnspan=1)
+        text.grid(row=row, column=column, rowspan=1, columnspan=1)
+        text.insert("1.0", "".join(open(f"{path}/{i}", "r")))
 
     nm.mainloop()
